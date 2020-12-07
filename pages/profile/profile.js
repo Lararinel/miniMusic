9aee6482954944bc84e2f72bd49a4f702db535ee
fuxiaoling <597540521@qq.com>
2020-12-07 20:32:40 +0800

    render

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -11,8 +11,10 @@ Page({
   data: {
     userInfo: null, //用户信息
     age: '', //用户年龄（几零后）
-    collectSongLists: [],  //用户收藏的歌单
     createSongLists: [],   //用户创建的歌单
+    collectSongLists: [],  //用户收藏的歌单
+    createListsLoves: 0, //创建的歌单被收藏次数
+    collectListsLoves: 0, //收藏的歌单被收藏次数
   },
   onLoad() {
     const userInfo = wx.getStorageSync('userInfo');
@@ -28,6 +30,7 @@ Page({
     this.getAge();
     this.getRadio();
     this.getUserSongLists();
+    this.computeLoves(this.data.createSongLists, this.data.collectSongLists);
   },
   // 获取年龄：几零后
   getAge() {
@@ -85,6 +88,28 @@ Page({
     } catch (error) {
 
     }
+  },
+  // 计算收藏次数
+  computeLoves(array1, array2) {
+    let newArray1 = [];
+    let newArray2 = [];
+    array1.forEach((list)=> {
+      if(list[subscribedCount]) {   //subscribedCount为每个歌单对象的收藏次数
+        newArray1.push(list[subscribedCount]);
+      }
+    })
+    array2.forEach((list)=> {
+      if(list[subscribedCount]) {
+        newArray2.push(list[subscribedCount]);
+      }
+    })
+    const createListsLoves = newArray1.reduce(function(prev, cur) {return prev + cur}, 0);
+    const collectListsLoves = newArray2.reduce(function(prev, cur) {return prev + cur}, 0);
+    this.setData({
+      createListsLoves,
+      collectListsLoves
+    })
+    console.log(createListsLoves, collectListsLoves)
   }
 
 })
